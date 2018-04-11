3f5ac097de76648c14bac2af6b9cf397f0456916
ashrakat <ashrakat.mokhtar@yahoo.com>
2018-04-11 19:04:56 +0200

    page 1&3&5 -Edit dataFiles

--- js/positions.js
+++ js/positions.js
@@ -1,44 +1,44 @@
 var positions = {
     "position_items" :[
         {
-            num : "1",
+            num : 1,
             date: "2015",
             title: "Mohammed Bin Zayed receives Qatari FM",
             description: "His Highness Sheikh Mohammed Bin Zayd Al Nahyan, Crown Prince of Abo Dhabi and Deputy Supreme Commander of the UAE Armed Force's, Today receives visiting Sheikh Mohammed Bin Abdulrahman Al-Thani, Qatar Minister of Foreign Affairs. at Al Shati Palace."
         },
         
          {
-            num : "2",
+            num : 2,
             date: "2015", 
             title: "Mohammed Bin Zayed receives Qatari FM",
             description: "His Highness Sheikh Mohammed Bin Zayd Al Nahyan, Crown Prince of Abo Dhabi and Deputy Supreme Commander of the UAE Armed Force's, Today receives visiting Sheikh Mohammed Bin Abdulrahman Al-Thani, Qatar Minister of Foreign Affairs. at Al Shati Palace."
         },
          {
-            num : "3",
+            num : 3,
             date: "2015", 
             title: "Mohammed Bin Zayed receives Qatari FM",
             description: "His Highness Sheikh Mohammed Bin Zayd Al Nahyan, Crown Prince of Abo Dhabi and Deputy Supreme Commander of the UAE Armed Force's, Today receives visiting Sheikh Mohammed Bin Abdulrahman Al-Thani, Qatar Minister of Foreign Affairs. at Al Shati Palace."
         },
          {
-            num : "4",
+            num : 4,
             date: "2015", 
             title: "Mohammed Bin Zayed receives Qatari FM",
             description: "His Highness Sheikh Mohammed Bin Zayd Al Nahyan, Crown Prince of Abo Dhabi and Deputy Supreme Commander of the UAE Armed Force's, Today receives visiting Sheikh Mohammed Bin Abdulrahman Al-Thani, Qatar Minister of Foreign Affairs. at Al Shati Palace."
         },
          {
-            num : "5",
+            num : 5,
             date: "2015", 
             title: "Mohammed Bin Zayed receives Qatari FM",
             description: "His Highness Sheikh Mohammed Bin Zayd Al Nahyan, Crown Prince of Abo Dhabi and Deputy Supreme Commander of the UAE Armed Force's, Today receives visiting Sheikh Mohammed Bin Abdulrahman Al-Thani, Qatar Minister of Foreign Affairs. at Al Shati Palace."
         },
          {
-            num : "6",
+            num : 6,
             date: "2015", 
             title: "Mohammed Bin Zayed receives Qatari FM",
             description: "His Highness Sheikh Mohammed Bin Zayd Al Nahyan, Crown Prince of Abo Dhabi and Deputy Supreme Commander of the UAE Armed Force's, Today receives visiting Sheikh Mohammed Bin Abdulrahman Al-Thani, Qatar Minister of Foreign Affairs. at Al Shati Palace."
         },
          {
-            num : "7",
+            num : 7,
             date: "2015", 
             title: "Mohammed Bin Zayed receives Qatari FM",
             description: "His Highness Sheikh Mohammed Bin Zayd Al Nahyan, Crown Prince of Abo Dhabi and Deputy Supreme Commander of the UAE Armed Force's, Today receives visiting Sheikh Mohammed Bin Abdulrahman Al-Thani, Qatar Minister of Foreign Affairs. at Al Shati Palace."
@@ -48,13 +48,13 @@ var positions = {
     
   "main_positions" : [
       {
-           num : "1",
+           num : 1,
            img_src: "switch_photos/5.jpg",
            title: "Mohammed Bin Zayed Mohammed Bin Zayed receives Qatari FM Mohammed Bin Zayed receives Qatari FM",
            description: "His Highness Sheikh Mohammed Bin Zayd Al Nahyan, Crown Prince of Abo Dhabi and Deputy Supreme Commander of the UAE Armed Force's, Today receives visiting Sheikh Mohammed Bin Abdulrahman Al-Thani, Qatar Minister of Foreign Affairs. at Al Shati Palace."
       },
        {
-           num : "2",
+           num : 2,
            img_src: "switch_photos/5.jpg",
            title: "Mohammed Bin Zayed Mohammed Bin Zayed receives Qatari FM Mohammed Bin Zayed receives Qatari FM",
            description: "His Highness Sheikh Mohammed Bin Zayd Al Nahyan, Crown Prince of Abo Dhabi and Deputy Supreme Commander of the UAE Armed Force's, Today receives visiting Sheikh Mohammed Bin Abdulrahman Al-Thani, Qatar Minister of Foreign Affairs. at Al Shati Palace."
