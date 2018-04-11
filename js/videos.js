c543a0e4aec07b3f89e6754d13376f907395dcf1
ashrakat <ashrakat.mokhtar@yahoo.com>
2018-04-11 18:02:36 +0200

    page 1&3&5

--- js/videos.js
+++ js/videos.js
@@ -1,38 +1,48 @@
 var videos = {
  "video_items" :[
    {
-    num:"2",   
+    num:"1",   
     src : "switch_photos/2.jpg",				
     description : "From the speech delivered at the student Forum  at 2007"
     }, 
     {
-    num:"3",     
+    num:"2",     
     src : "switch_photos/3.jpg",
     description : "From the speech delivered at the student Forum  at 2007"
     }, 
     {
-     num:"4",     
+     num:"3",     
      src: "switch_photos/4.jpg",
      description : "From the speech delivered at the student Forum  at 2007"
     },
      {	
-     num:"5",      
+     num:"4",      
      src : "switch_photos/5.jpg",
      description : "From the speech delivered at the student Forum  at 2007"
    },
     {
-     num:"6",     
+     num:"5",     
      src : "switch_photos/6.jpg",
      description : "From the speech delivered at the student Forum  at 2007"
    },
  {	
-     num:"7",  
+     num:"6",  
      src : "switch_photos/7.jpg",
      description : "From the speech delivered at the student Forum  at 2007"
    },
    {
-     num:"8",    
+     num:"7",    
      src: "switch_photos/8.jpg",
+     description : "From the speech delivered at the student Forum  at 2007"
+   },
+     {	
+     num:"8",      
+     src : "switch_photos/5.jpg",
+     description : "From the speech delivered at the student Forum  at 2007"
+   },
+    {
+     num:"9",     
+     src : "switch_photos/6.jpg",
      description : "From the speech delivered at the student Forum  at 2007"
    }
   ],
